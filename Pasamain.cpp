bbcf403f5a18e6e20372fceb74a716d45af0b1ec
mdpasa-sys <mdpasa@gbox.adnu.edu.ph>
2025-08-08 14:40:23 +0800

    Update Pasamain.cpp

--- Pasamain.cpp
+++ Pasamain.cpp
@@ -9,7 +9,7 @@ int main (){
   cin >> num1; // user input number
   cout<< "Enter another number: "; // output another number
   cin >> num2; // use another number
-  product = num1 * num2; // the value of the sum num1 + num2
+  product = num1 * num2; // the value of the sum num1 * num2
   cout<< "The product of the two number is: "<< product; // output product of number
     
     return  0;
