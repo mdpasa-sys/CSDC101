d8884c7f93fe754c53948e26eb964b638edf393d
mdpasa-sys <mdpasa@gbox.adnu.edu.ph>
2025-08-08 14:12:16 +0800

    Create Pasamain.cpp
    Added main.cpp

---
+++ Pasamain.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // input and outpurt libary
+using namespace std; // standard namespace for cin and cout
+
+int main (){
+    
+  int num1,num2,product; // interger of the variables
+  
+  cout<<"Enter a number: "; // output of the Enter number
+  cin >> num1; // user input number
+  cout<< "Enter another number: "; // output another number
+  cin >> num2; // use another number
+  product = num1 * num2; // the value of the sum num1 + num2
+  cout<< "The product of the two number is: "<< product; // output product of number
+    
+    return  0;
